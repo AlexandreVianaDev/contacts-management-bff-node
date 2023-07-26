c05f06ddaa67dce246a7fa0aed1657e41a3c4a30
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-07-26 18:56:42 -0300

    feat: autologin route

--- src/services/users/loginUser.service.ts
+++ src/services/users/loginUser.service.ts
@@ -1,39 +1,38 @@
 import { compare } from "bcryptjs";
 import { AppError } from "../../error";
-import { IToken, TUserLogin } from "../../interfaces/users.interfaces";
+import { ILoginResponse, TUserLogin } from "../../interfaces/users.interfaces";
 import { sign } from "jsonwebtoken";
 import { AppDataSource } from "../../data-source";
 import { User } from "../../entities";
 import { Repository } from "typeorm";
+import { userWithoutPasswordSchema } from "../../schemas/users.schemas";
 
 export const loginUserService = async (
   userData: TUserLogin
-): Promise<IToken> => {
+): Promise<ILoginResponse> => {
   const emailBody: string = userData.email;
 
   const userRepo: Repository<User> = AppDataSource.getRepository(User);
 
-  const user: User | null = await userRepo.findOneBy({
+  const userFromRepo: User | null = await userRepo.findOneBy({
     email: emailBody,
   });
 
   const passwordMatch: boolean = await compare(
     userData.password,
-    user!.password
+    userFromRepo!.password
   );
 
   if (!passwordMatch) {
     throw new AppError("Invalid credentials", 401);
   }
 
-  const token: string = sign(
-    {},
-    String(process.env.SECRET_KEY),
-    {
-      expiresIn: String(process.env.EXPIRES_IN || "24h"),
-      subject: String(user!.id),
-    }
-  );
+  const token: string = sign({}, String(process.env.SECRET_KEY), {
+    expiresIn: String(process.env.EXPIRES_IN || "24h"),
+    subject: String(userFromRepo!.id),
+  });
 
-  return { token };
+  const user = userWithoutPasswordSchema.parse(userFromRepo);
+
+  return { token, user };
 };
